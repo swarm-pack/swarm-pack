fc1eea339960ebe5ed3b54054d6afa2f8edc295e
vudknguyen <vu.nguyen.ags@gmail.com>
2019-02-27 22:24:48 +0800

    Fix styling issue & clean unused bash script

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: 2
+      }
+    ]
+  ]
+};
