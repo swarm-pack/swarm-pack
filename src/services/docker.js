9d222e5a24842607e2b017a5eebf2dbbe0ae33f6
vudknguyen <vu.nguyen.ags@gmail.com>
2019-03-10 17:32:54 +0800

    Refactor config area

--- src/services/docker.js
+++ src/services/docker.js
@@ -1,6 +1,6 @@
 const Docker = require('dockerode');
-const { spawn } = require('child_process');
 const config = require('../config').get();
+const { pipeableSpawn } = require('../utils');
 
 function getDockerodeClient() {
   if (config.docker.host) {
@@ -15,45 +15,6 @@ function getDockerodeClient() {
 
 function getDockerArgs() {
   return ['-H', config.docker.url];
-}
-
-function pipeableSpawn(stream, command, args, onExit, onError, onStdout, onStderr) {
-  const child = spawn(command, args, { env: process.env });
-  if (stream) {
-    stream.pipe(child.stdin);
-  }
-
-  child.on('exit', (code, signal) => {
-    if (onExit) {
-      onExit(code, signal);
-    } else {
-      console.log(`Process exited with code ${code} and signal ${signal}`);
-    }
-  });
-
-  child.on('error', err => {
-    if (onError) {
-      onError(err);
-    } else {
-      console.log(`Process exited with error ${err}`);
-    }
-  });
-
-  child.stdout.on('data', data => {
-    if (onStdout) {
-      onStdout(data);
-    } else {
-      console.log(`Process stdout:\n${data}`);
-    }
-  });
-
-  child.stderr.on('data', data => {
-    if (onStderr) {
-      onStderr(data);
-    } else {
-      console.log(`Process stderr:\n${data}`);
-    }
-  });
 }
 
 function pipeToDocker(stream, args, onExit, onError, onStdout, onStderr) {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const fs = require('fs-extra');
+const { spawn } = require('child_process');
 
 /**
  * Get a property of a (nested) object using a dot-notation string
@@ -10,12 +11,70 @@ function getObjectProperty(property, object) {
 }
 
 function readFile(fileName, type = 'utf-8') {
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     fs.readFile(fileName, type, (err, data) => (err ? reject(err) : resolve(data)));
+  });
+}
+
+function ensurePathExisted(path, isDir = false) {
+  if (isDir) {
+    fs.ensureDirSync(path);
+  } else {
+    fs.ensureFileSync(path);
+  }
+}
+
+function isFileEmpty(path) {
+  if (!fs.pathExistsSync(path)) {
+    throw new Error(`${path} is not existed`);
+  }
+
+  return fs.statSync(path).size === 0;
+}
+
+function pipeableSpawn(stream, command, args, onExit, onError, onStdout, onStderr) {
+  const child = spawn(command, args, { env: process.env });
+  if (stream) {
+    stream.pipe(child.stdin);
+  }
+
+  child.on('exit', (code, signal) => {
+    if (onExit) {
+      onExit(code, signal);
+    } else {
+      console.log(`Process exited with code ${code} and signal ${signal}`);
+    }
+  });
+
+  child.on('error', err => {
+    if (onError) {
+      onError(err);
+    } else {
+      console.log(`Process exited with error ${err}`);
+    }
+  });
+
+  child.stdout.on('data', data => {
+    if (onStdout) {
+      onStdout(data);
+    } else {
+      console.log(`Process stdout:\n${data}`);
+    }
+  });
+
+  child.stderr.on('data', data => {
+    if (onStderr) {
+      onStderr(data);
+    } else {
+      console.log(`Process stderr:\n${data}`);
+    }
   });
 }
 
 module.exports = {
   getObjectProperty,
-  readFile
+  readFile,
+  ensurePathExisted,
+  pipeableSpawn,
+  isFileEmpty
 };
